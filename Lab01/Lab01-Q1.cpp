b3c445de9f19feee91b91e5eb217f380141e49f7
113310206 <t113310206@ntut.org.tw>
2025-02-26 22:44:46 +0800

    Update Lab01-Q1.cpp

--- Lab01/Lab01-Q1.cpp
+++ Lab01/Lab01-Q1.cpp
@@ -1,31 +1,40 @@
+
 class Solution {
     public:
         bool isMonotonic(vector<int>& nums) {
+            int count = 0;
             bool ins = 1, dec = 1;
             // count +2 for the assignment
-    
+            count = count+2;
             for(size_t i = 1; i < nums.size(); i++)
             {
                 // count +1 for the comparison
                 // count +1 for the increment
                 // count +1 for the initialization
+                count = count + 3;
                 if(nums[i] < nums[i - 1])
                 {
                     // count +1 for the comparison
                     // count +1 for the assignment
+                    count = count + 2;
                     dec = 0;
                     // count +1 for the assignment
+                    count++;
                 }
                 if(nums[i] > nums[i -1])
                 {
                     // count +1 for the comparison
                     // count +1 for the assignment
+                    count = count + 2;
                     ins = 0;
                     // count +1 for the assignment
+                    count++;
                 }
             }
             return ins || dec;
             // count +1 for the return
             // count +1 for the OR
+            count = count + 2;
+            return count;
         }
     };
